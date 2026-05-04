f6ddf4a542dc8573ad02cbfae304b9029babe1fa
Aapo Katila <abolakatila@gmail.com>
2026-05-04 11:25:12 +0300

    fixes v3

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cmath>
 #include <map>
 
 #include "raylib.h"
@@ -25,6 +25,9 @@ Shift winner = None;
 void setup(int gameWidth, int cubeWidth, int cubeHeight);
 bool isInside(int gameWidth);
 bool checkWin(int gameWidth, Shift currentPlayer);
+bool isFull(int gameWidth);
+
+bool hasWinner();
 
 int main()
 {
@@ -36,6 +39,10 @@ int main()
     const int cubeWidth = windowWidth / gameWidth;
     const int cubeHeight = windowHeight / gameWidth;
     setup(gameWidth, cubeWidth, cubeHeight);
+    Image image = LoadImage("tic-tac-toe.png");
+    SetConfigFlags(FLAG_WINDOW_HIGHDPI);
+    SetWindowIcon(image);
+    UnloadImage(image);
     while (!WindowShouldClose())
     {
         BeginDrawing();
@@ -55,26 +62,40 @@ int main()
             }
             DrawRectangleV(Vector2(value.rect.x, value.rect.y), Vector2(value.rect.width, value.rect.height), color);
         }
-        DrawText(currentTurn == FirstPlayer ? "First Player's Turn" : "Second Player's Turn", 10, 10, 20, DARKGRAY);
 
+        Color winColor = { 0, 255, 136, 255 };
         if (winner == SecondPlayer)
         {
-            DrawText("Second Player Wins! Press R to Restart", 10, 40, 20, DARKGRAY);
+            DrawText("WHITE Won! Press R to Restart", 36, 40, 38, winColor);
         } else if (winner == FirstPlayer)
         {
-            DrawText("First Player Wins! Press R to Restart", 10, 40, 20, DARKGRAY);
+            DrawText("BLACK Won! Press R to Restart", 36, 40, 38, winColor);
+        }
+
+        bool full = isFull(gameWidth);
+        if (full) {
+            DrawText("Press R to Restart", 36, 40, 38, winColor);
         }
 
         EndDrawing();
-        isInside(gameWidth);
         if (IsKeyPressed(KEY_R))
         {
             setup(gameWidth, cubeWidth, cubeHeight);
         }
+
+        if (!hasWinner()) {
+            isInside(gameWidth);
+        }
+
+
     }
 
     return 0;
 
+}
+
+bool hasWinner() {
+    return winner != None;
 }
 
 bool checkWin(const int gameWidth, const Shift currentPlayer)
@@ -171,8 +192,23 @@ bool checkWin(const int gameWidth, const Shift currentPlayer)
     return false;
 }
 
+bool isFull(const int gameWidth) {
+    int claimedCount = 0;
+
+    for (auto d : positions) {
+        if (d.second.claimed) {
+            claimedCount++;
+        }
+    }
+    if (claimedCount == (gameWidth*gameWidth)) {
+        return true;
+    }
+    return false;
+}
+
 bool isInside(const int gameWidth)
 {
+
     Vector2 mousePosition = GetMousePosition();
     for (auto& [key, value] : positions)
     {
